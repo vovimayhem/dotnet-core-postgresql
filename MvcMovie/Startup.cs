e32b119bc8d0b7b742ffe512421a5d766d668460
LeeTrent <Lee.Trent.1@gmail.com>
2018-01-30 22:08:15 -0600

    Add connectivity configuration and logic for PostgeSQL

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -22,6 +26,39 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            ////////////////////////////////////////////////////////////////////////
+            // WRITE ENVIRONMENT VARIABLES TO THE LOG
+            ////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("List of environment variables:");
+            var enumerator = Environment.GetEnvironmentVariables().GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine($"{enumerator.Key,5}:{enumerator.Value,100}");
+            }
+            ////////////////////////////////////////////////////////////////////////
+
+            String connectionString = null;
+
+            // CHECK FOR LOCAL 'LOCAL_CONNECTION_STRING' ENVIRONMENT VARIABLE
+            // (in case we're running on localhost)
+            connectionString = Environment.GetEnvironmentVariable("LOCAL_CONNECTION_STRING");
+ 
+            // IF connectionString IS NULL, WE MUST BE RUNNING IN THE CLOUD,
+            // (either CLOUD.GOV or Cloud Foundry)
+            if (connectionString == null) {
+                connectionString = BuildConnectionString();
+            }
+  
+            // WRITE CONNECTION STRING TO THE LOG
+            Console.WriteLine("Connection String:" + connectionString);
+
+            // NOW THAT WE HAVE OUR CONNECTION STRING
+            // WE CAN ESTABLISH OUR DB CONTEXT
+            services.AddDbContext<MvcMovieContext>
+            (
+		        opts => opts.UseNpgsql(connectionString)
+		    );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,5 +82,48 @@ namespace MvcMovie
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+
+        public String BuildConnectionString()
+        {
+            String connectionString = null;
+                               
+			//This section retrieves credentials from the VCAP_SERVICES
+			try
+			{
+				string vcapServices = System.Environment.GetEnvironmentVariable("VCAP_SERVICES");
+
+				if (vcapServices != null)
+				{
+					dynamic json = JsonConvert.DeserializeObject(vcapServices);
+					foreach (dynamic obj in json.Children())
+					{
+
+						dynamic credentials = (((JProperty)obj).Value[0] as dynamic).credentials;
+						if (credentials != null)
+						{
+							string host = credentials.host;
+							string username = credentials.username;
+							string password = credentials.password;
+                            string port = credentials.port;
+                            string db_name = credentials.db_name;
+
+                            connectionString = "Username=" + username + ";"
+                                + "Password=" + password + ";"
+                                + "Host=" + host + ";"
+                                + "Port=" + port + ";"
+                                + "Database=" + db_name + ";Pooling=true;";
+
+                            return connectionString;
+						}
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception in Startup.BuildConnectionString:");
+				Console.WriteLine(e);
+            }
+            return "No Connection String";
+		}
     }
 }
