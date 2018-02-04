a4fd0d2e1d5a0b591e06cfd271ad2549f8a27d1f
LeeTrent <Lee.Trent.1@gmail.com>
2018-02-04 08:48:49 -0600

    Adds validation logic to 'Movie' model

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,19 +6,32 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
-        
-        [Display(Name = "Movie Title")]
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
-        
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        
-        public string Genre { get; set; }
-        
-        public string Rating { get; set; }
-        
-        public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+ 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; } 
     }
+
+
+
+
+
 }
