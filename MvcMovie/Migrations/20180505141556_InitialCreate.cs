b9a582e5c8719c8d34358b73f5e9cb2cea40cb9b
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-05-05 09:17:39 -0500

    Added migrations

---
+++ MvcMovie/Migrations/20180505141556_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MvcMovie.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Genre = table.Column<string>(maxLength: 30, nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Rating = table.Column<string>(maxLength: 5, nullable: false),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
--- MvcMovie/dev-entrypoint.sh
+++ MvcMovie/dev-entrypoint.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
 # 1: Ensure we've got all the dependencies
+echo "=== Restoring packages..."
 dotnet restore
+
+# 2: Ensure we've got our database up to date:
+echo "=== Updating the database..."
+dotnet ef database update
 
 # 2: Run the given command:
 exec "$@"
